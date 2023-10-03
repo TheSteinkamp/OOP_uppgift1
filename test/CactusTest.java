ede85c400806829600ec23591635a875a51c6cfe
Jessica Steinkamp <jessica@steinkamp.se>
2023-10-03 18:10:44 +0200

    Uppdaterad version med test

---
+++ test/CactusTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CactusTest {
+
+    @Test
+    void howMuchFeed() {
+    }
+
+    @Test
+    void printOut() {
+    }
+
+    @Test
+    void wichFluid() {
+    }
+}
